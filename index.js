4182d45a13e61cfb71b2b2bfee8fe5c4fe1e9c0c
kimtaehyung <taehyeong77@naver.com>
2020-01-12 01:50:07 +0900

    commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const electron = require('electron');
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            { label: 'New Todo' },
+            { label: 'Quit', 
+            /*
+            accelerator: (() => {
+                if(process.platform === 'darwin') {
+                    return 'Command+Q';
+                } else {
+                    return 'Ctrl+Q'
+                }
+            })(),
+            */
+           accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q', 
+            click() {
+                app.quit();
+            }}
+        ]
+    }
+];
+
+if(process.platform === 'darwin') {  // OSX
+    menuTemplate.unshift({});          // for Menubar shift
+}
